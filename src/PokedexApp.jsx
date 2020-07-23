a7075d03050420de9f7ae2d53a71eb2927e4ff5a
Jonathan Ephraim Edwin <joedwin.rpl@gmail.com>
2020-07-23 17:56:23 +0700

    add router

--- src/PokedexApp.jsx
+++ src/PokedexApp.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import List from './components/List/List';
 import Detail from './components/Detail/Detail';
 
 function PokedexApp() {
   return (
     <div className="container py-3">
-      <h1>Pokedex</h1>
-      <div className="row">
-        <div className="col-2">
-          Filter Here
+
+      <Router>
+        <div>
+          <Link to="/">
+            <h1>Pokedex</h1>
+          </Link>
+
+          <Switch>
+            <Route exact path="/">
+              <List />
+            </Route>
+            <Route path="/:name" children={<Detail />} />
+          </Switch>
+
         </div>
-        <div className="col-10">
-          <List />
-          <Detail />
-        </div>
-      </div>
+      </Router>
     </div>
   );
 }
