fc38c9e4bb515819dacab3fa43a69eff0b31b88c
Jonathan Ephraim Edwin <joedwin.rpl@gmail.com>
2020-07-23 18:03:40 +0700

    add style

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { usePokemons } from './hooks';
 
+const listStyle = { width: '18rem' };
+
 export default function List() {
   const pokemons = usePokemons();
 
   const listItems = pokemons.data.map((pokemon) => (
-    <li key={pokemon.name}>
+    <li className="list-group-item" key={pokemon.name}>
       <Link to={`/${pokemon.name}`}>{pokemon.name}</Link>
     </li>
   ));
 
-  return <ul>{listItems}</ul>;
+  return (
+    <div className="card" style={listStyle}>
+      <ul className="list-group list-group-flush">{listItems}</ul>
+    </div>
+  );
 }
