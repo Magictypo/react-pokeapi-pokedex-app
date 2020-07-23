f3207c8f5830fd6be6fcef41e2713223a5ba7fb8
Jonathan Ephraim Edwin <joedwin.rpl@gmail.com>
2020-07-23 17:57:46 +0700

    add link to detail page

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { usePokemons } from './hooks';
 
 export default function List() {
@@ -6,7 +7,7 @@ export default function List() {
 
   const listItems = pokemons.data.map((pokemon) => (
     <li key={pokemon.name}>
-      {pokemon.name}
+      <Link to={`/${pokemon.name}`}>{pokemon.name}</Link>
     </li>
   ));
 
